251da9683d2e4d143bcc39747e598ed147321101
Zachary Cecchetti <zcecchetti@gmail.com>
2022-04-12 13:51:24 -0700

    Continued styling page, added greeting for new users, when user moves between tabs, page will now populate all existing tasks per project

--- src/createProjectTab.js
+++ src/createProjectTab.js
@@ -2,21 +2,31 @@
 export default function generateTab(projectInList) {
 
     const container = document.getElementById("content");
+    const taskTab = document.createElement("div");
+    taskTab.setAttribute("id", "taskTab");
+    container.appendChild(taskTab);
+
     const listName = document.createElement("div");
     listName.setAttribute("id", "listName");
     listName.textContent = projectInList.projectName;
-    container.appendChild(listName);
+    taskTab.appendChild(listName);
 
     const addTaskButton = document.createElement("button");
     addTaskButton.setAttribute("id", "addTaskButton");
     addTaskButton.textContent = "+ Add New Task";
-    container.appendChild(addTaskButton);
+    taskTab.appendChild(addTaskButton);
 
     const taskContainer = document.createElement("div");
     taskContainer.setAttribute("id", "taskContainer");
-    container.appendChild(taskContainer);
+    taskTab.appendChild(taskContainer);
 
-    
+    if (projectInList.taskList.length > 0) {
+
+        for (let i = 0; i < projectInList.taskList.length; i++) {
+
+            addTaskToDOM(projectInList, i);
+        };
+    };
 
     addTaskButton.addEventListener("click", () => {
 
@@ -30,17 +40,27 @@ export default function generateTab(projectInList) {
 export function deleteTab() {
 
     const container = document.getElementById("content");
-    const listName = document.getElementById("listName");
-    if (listName) {
-        container.removeChild(listName);
-    };
+    const taskTab = document.getElementById("taskTab");
+    const userGreeting = document.getElementById("userGreeting");
+    if (taskTab) {
+        container.removeChild(taskTab);
+    } else {
+        container.removeChild(userGreeting);
+    }
 };
 
 function addTaskToDOM(projectInList, taskID) {
 
+    const currentTask = projectInList.taskList[taskID];
     const container = document.getElementById("taskContainer");
     const newTask = document.createElement("div");
     newTask.setAttribute("id", `${taskID}`);
-    newTask.textContent = projectInList.taskList[taskID].title;
+    newTask.textContent = currentTask.title;
+    newTask.classList.add("projectTask")
+
+    const taskInfo = document.createElement("div");
+    taskInfo.textContent = `${currentTask.summary} | Due: ${currentTask.dueDate}`;
+    taskInfo.classList.add("taskInfo");
+    newTask.appendChild(taskInfo);
     container.appendChild(newTask);
 };

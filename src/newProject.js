76320eea425b48e066e875812cad1b76cd29f485
Zachary Cecchetti <zcecchetti@gmail.com>
2022-04-12 11:43:08 -0700

    Added function to newProject object to add tasks to tasklist

--- src/newProject.js
+++ src/newProject.js
@@ -2,9 +2,22 @@
 function newProject(id) {
 
     const taskList = [ ];
+    let taskCount = 0;
+    const addTask = function() {
+
+        const title = prompt("what do you need to do? ");
+        const dueDate = prompt("when is this due?");
+        // can later put a variable here to check how many days are left
+        const taskID = taskCount;
+        const summary = prompt("what are the steps to complete this? ");
+        const priority = prompt("is this a high priority item? ");
+        const todoItem = {title, taskID, summary, dueDate, priority}
+        taskList.push(todoItem);
+        console.log(taskList, "\n", todoItem);
+    };
     const projectID = id;
     const projectName = prompt("project name? ");
-    return {projectName, projectID, taskList};
+    return {projectName, projectID, taskList, addTask};
 };
 
 function addProjectToDOM(project) {

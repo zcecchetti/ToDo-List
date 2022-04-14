ee516824f268e468741269bdb74f6fe469e67360
Zachary Cecchetti <zcecchetti@gmail.com>
2022-04-14 00:21:34 -0700

    Form to get project name now operational

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import './style.css';
-import {newProject, addProjectToDOM, projectForm} from "./newProject";
-
-const projectList = [ ];
-let projectCount = 0;
+import {projectForm} from "./newProject";
 
 const addProjectButton = document.getElementById("addProjectButton");
 addProjectButton.addEventListener("click", () => {
 
     projectForm();
-    const project = newProject(projectCount);
-    projectList.push(project);
-    addProjectToDOM(projectList[projectCount]);
-    projectCount++;
+    // const projectName = getProjectFormInfo();
+    // const project = newProject(projectName);
+    // projectList.push(project);
+    // addProjectToDOM(projectList[projectList.length - 1]);
+    // projectCount++;
 });
--- src/newProject.js
+++ src/newProject.js
@@ -1,28 +1,26 @@
 import generateTab, { deleteTab } from "./createProjectTab";
 
-function newProject(id) {
+const projectList = [ ];
+
+function newProject(projectName) {
 
     const taskList = [ ];
-    let taskCount = 0;
-    const projectID = id;
-    const projectName = prompt("project name? ");
+    const projectID = `project${projectList.length}`;
+    // const projectName = prompt("project name? ");
     function addTask() {
 
         const title = prompt("what do you need to do? ");
         const dueDate = prompt("when is this due?");
         // can later put a variable here to check how many days are left
-        const taskID = taskCount;
+        const taskID = "task" + taskList.length;
         const summary = prompt("what are the steps to complete this? ");
         const priority = prompt("is this a high priority item? ");
         const todoItem = {title, taskID, summary, dueDate, priority}
         taskList.push(todoItem);
-        taskCount++
         console.log(taskList, "\n", todoItem);
     };
     return {projectName, projectID, taskList, addTask};
 };
-
-
 
 function addProjectToDOM(project) {
 
@@ -39,21 +37,50 @@ function addProjectToDOM(project) {
     });
 }
 
+window.createProject = function() {
+
+    const projectName = document.getElementById("projectName").value;
+    const project = newProject(projectName);
+    projectList.push(project);
+    const index = findIndex(project)
+    addProjectToDOM(projectList[index]);
+    // console.log(projectList[index]);
+    removeForm();
+};
+
+function findIndex(project) {
+
+    for (let i = 0; i < projectList.length; i++) {
+
+        if (projectList[i].projectID === project.projectID) {
+            return i
+        }
+    };
+};
+
 function projectForm() {
 
     const container = document.getElementById("sidebarContainer");
     const addProjectForm = document.createElement("form");
     addProjectForm.setAttribute("id", "projectForm");
+    addProjectForm.setAttribute("onsubmit", `createProject(); return false`)
+
     const inputName = document.createElement("input");
     inputName.setAttribute("type", "text");
     inputName.setAttribute("id", "projectName");
     inputName.setAttribute("name", "projectName");
-    inputName.setAttribute("maxlength", "45");
+    inputName.setAttribute("maxlength", "30");
     inputName.setAttribute("required", "");
 
     addProjectForm.appendChild(inputName);
     container.appendChild(addProjectForm);
-
 };
 
-export {newProject, addProjectToDOM, projectForm};
+function removeForm() {
+
+    const formRemove = document.getElementById("projectForm");
+    const container = document.getElementById("sidebarContainer");
+    container.removeChild(formRemove);
+};
+
+export {projectForm};
